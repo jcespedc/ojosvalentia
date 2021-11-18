557e2ca48cc64626933d0e1388b3471178359d50
Juan Sebastian Céspedes Saldarriaga <cbas209@gmail.com>
2021-11-17 21:28:49 -0500

    mi primer commit

---
+++ pages/index.php
@@ -0,0 +1,142 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/normalize.css">
+    <title>Ojos De Valentia</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Nunito:ital,wght@0,400;1,700&display=swap" rel="stylesheet">
+    
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+</head>
+<body style="background-color:#FAFDFF">
+    <header>
+        <h1 class="text-center">Ojos De <span style="color:#0dcaf0">Valentia</span></h1>
+    </header>
+        <div class="container  " style="background-color: #e3f2fd; font-size: 20px;">
+            <ul class="nav nav-pills nav-fill">
+                <li class="nav-item">
+                    <a class="nav-link " href="#">Inicio</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Nosotros</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Catalogo</a>
+                </li>
+                
+            </ul>
+        </div>
+        <br>
+    <div class="container modal-content">    
+    <section class="container ">
+        <div style="border:20px">
+            <img src="../img/Captura.PNG" class="mx-auto d-block">
+                <h2 class="subtitulo">¡Un sueño se anhela, un proyecto se trabaja!👀🙌🏻💙<span>ALCANCÍAS</span>💰💵</h2>
+                <div class="row justify-content-center">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-map-pin" width="88" height="88" viewBox="0 0 24 24" stroke-width="1.5" stroke="#00abfb" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                    <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+                    <circle cx="12" cy="11" r="3" />
+                    <path d="M17.657 16.657l-4.243 4.243a2 2 0 0 1 -2.827 0l-4.244 -4.243a8 8 0 1 1 11.314 0z" />
+                    </svg>
+                </div>
+              
+            <p class="text-center">🎨100% Hecho a mano, diseños exclusivos.🇨🇴Envíos a toda #Colombia</p>
+            <div class="text-center">
+            <a href="./contactanos.html" class="boton">Contactanos</a>
+            </div>
+        </div>        
+    </section>
+    <br>
+    <main class="container ">
+        <h2 class="text-center"style="color:#0dcaf0">Mis Servicios</h2>
+        <br>
+        <div class="row">
+        <section class="col-sm-4">
+            <h3 class="text-center">Viajes</h3>
+            <div class="row justify-content-center">
+            <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-plane-departure" width="40" height="40" viewBox="0 0 24 24" stroke-width="1.5" stroke="#00abfb" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+                <path d="M15 12h5a2 2 0 0 1 0 4h-15l-3 -6h3l2 2h3l-2 -7h3z" transform="rotate(-15 12 12) translate(0 -1)" />
+                <line x1="3" y1="21" x2="21" y2="21" />
+              </svg>
+            </div>  
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem fugiat impedit et explicabo,
+            ipsa dicta molestiae alias accusamus tenetur maiores. Ratione sequi eos velit nulla.</p>
+        </section> 
+        <section class="col-sm-4" >
+            <h3 class="text-center">Inversiones Felices</h3>
+            <div style="align-items: center;">
+            <div class="row justify-content-center">
+            <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-coin" width="40" height="40" viewBox="0 0 24 24" stroke-width="1.5" stroke="#00abfb" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+                <circle cx="12" cy="12" r="9" />
+                <path d="M14.8 9a2 2 0 0 0 -1.8 -1h-2a2 2 0 0 0 0 4h2a2 2 0 0 1 0 4h-2a2 2 0 0 1 -1.8 -1" />
+                <path d="M12 6v2m0 8v2" />
+              </svg>
+            </div>  
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem fugiat impedit et explicabo,
+                ipsa dicta molestiae alias accusamus tenetur maiores. Ratione sequi eos velit nulla.</p>
+        </section>
+        <section class="col-sm-4">
+            <h3 class="text-center">Nuestro Proceso</h3>
+            <div class="row justify-content-center">
+            <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-palette" width="40" height="40" viewBox="0 0 24 24" stroke-width="1.5" stroke="#00abfb" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+                <path d="M12 21a9 9 0 1 1 0 -18a9 8 0 0 1 9 8a4.5 4 0 0 1 -4.5 4h-2.5a2 2 0 0 0 -1 3.75a1.3 1.3 0 0 1 -1 2.25" />
+                <circle cx="7.5" cy="10.5" r=".5" fill="currentColor" />
+                <circle cx="12" cy="7.5" r=".5" fill="currentColor" />
+                <circle cx="16.5" cy="10.5" r=".5" fill="currentColor" />
+              </svg>
+            </div>  
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem fugiat impedit et explicabo,
+                ipsa dicta molestiae alias accusamus tenetur maiores. Ratione sequi eos velit nulla.</p>
+        </section>
+        </div>
+        
+    </main>
+    <hr>
+    <section class="container ">
+        <h2 class="text-center" >Contacto</h2>
+        <hr>
+        <form action="/action_page.php" class=" row">
+            <div class="col-sm-6">
+                <label for="email" class="form-label">Nombre </label>
+                <input type="text" class="form-control" id="email" placeholder="Enter Name" name="email">
+            </div>
+            <div class="col-sm-6">
+                <label for="pwd" class="form-label">Apellido</label>
+                <input type="text" class="form-control" id="pwd" placeholder="Enter LastName" name="pswd">
+                <br>
+            </div>
+           
+            <div class="col-sm-6">
+                <label for="email" class="form-label">Telefono</label>
+                <input type="number" class="form-control" id="email" placeholder="Enter Telephone" name="email">
+            </div>
+            <div class="col-sm-6">
+                <label for="pwd" class="form-label">E-mail</label>
+                <input type="email" class="form-control" id="pwd" placeholder="Enter Email" name="pswd">
+                <br>
+            </div>
+            <div class="form-check mb-4">
+                <label class="form-check-label">
+                <input class="form-check-input" type="checkbox" name="remember"> Remember me
+                </label>
+            </div>
+
+            <div class="">
+            <button type="submit" class="btn btn-primary">Enviar</button>
+            </div>
+            
+        </form>
+    </section>
+    <footer>
+        <p>Todos Los derechos Reservados. Valentina Barrera Medina</p>
+    </footer>    
+    </div>
+</body>
+</html>
